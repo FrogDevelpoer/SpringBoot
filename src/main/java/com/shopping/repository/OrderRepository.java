a3db611260fede49ee57bccbe94922f247f812c7
FrogDeveloper <97085193+FrogDevelpoer@users.noreply.github.com>
2022-06-10 09:20:44 +0900

    Revert "Changes"
    This reverts commit ed2c4f8341002e90c93475daf98695c81855c343.

--- src/main/java/com/shopping/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopping.repository;
-
-import com.shopping.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
